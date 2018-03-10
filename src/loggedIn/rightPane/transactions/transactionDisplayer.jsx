742d60ee45af177082c36e6ee3fd2e1e2a34dca9
lakhara <siddharth.lakhara@gmail.com>
2018-03-10 13:45:49 +0530

    restructuring table

--- src/loggedIn/rightPane/transactions/transactionDisplayer.jsx
+++ src/loggedIn/rightPane/transactions/transactionDisplayer.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import ReactTable from 'react-table';
 import './transactionDisplayer.css';
 
-const TableHead = props => (
-  props.head.map(e => (
-    <th>
-      {e}
-    </th>
-  ))
-);
+const TableHead = (props) => {
+  const { head } = props;
+
+  if (props.mode === 2) {
+    const index = head.indexOf('Sent To');
+    head.splice(index, 1);
+  } else if (props.mode === 1) {
+    const index = head.indexOf('Sent By');
+    head.splice(index, 1);
+  }
+
+  const AllHeadColumns = head.map((e) => {
+    const headColumn = <th>{e}</th>;
+    return headColumn;
+  });
+
+  return (
+    <tr>
+      {AllHeadColumns}
+    </tr>
+  );
+};
+
 
 const TableData = (props) => {
   const rows = [];
-  for (let i = 0; i < props.data.length; i += 1) {
+  const currentUser = 1;
+  let { data } = props;
+  if (props.mode === 1) { // display sent transactions
+    data = data.filter(row => (row.fromUser === currentUser));
+  } else if (props.mode === 2) {
+    data = data.filter(row => (row.toUser === currentUser));
+  }
+
+  for (let i = 0; i < data.length; i += 1) {
     const rowele = [];
-    for (const key in props.data[i]) {
-      if (props.data[i].hasOwnProperty(key)) {
-        const cell = <td> {props.data[i][key]} </td>;
+    for (const key in data[i]) {
+      if (data[i].hasOwnProperty(key)) {
+        const cell = <td> {data[i][key]} </td>;
         rowele.push(cell);
       }
     }
@@ -36,114 +59,25 @@ const TransactionsDisplayer = (props) => {
   }];
 
   const headAll = ['Sent To', 'Sent By', 'Amount', 'Status', 'Transaction Id'];
+  const headers = (
+    <TableHead
+      mode={props.displayTab}
+      head={headAll}
+    />
+  );
+  const allRows = (
+    <TableData
+      mode={props.displayTab}
+      data={dataAll}
+    />
+  );
 
-  const columnsAll = [{
-    Header: 'Sent To',
-    accessor: 'toUser',
-  }, {
-    Header: 'Sent by',
-    accessor: 'fromUser',
-  }, {
-    Header: 'Amount',
-    accessor: 'amount',
-    Cell: props => (<span>{props.value}</span>),
-  }, {
-    Header: 'Status',
-    accessor: 'status',
-  }, {
-    Header: 'transaction Id',
-    accessor: 'transactionId',
-  }];
-
-  const columnsSent = [{
-    Header: 'Sent to',
-    accessor: (d) => {
-      const sentArray = d.filter(e => e.fromUser === 'user1');
-      return sentArray.toUser;
-    },
-  }, {
-    Header: 'Amount',
-    accessor: 'amount',
-  }, {
-    Header: 'Status',
-    accessor: 'status',
-  }, {
-    Header: 'transaction Id',
-    accessor: 'transactionId',
-  }];
-
-  const columnsRcvd = [{
-    Header: 'Sent By',
-    accessor: (d) => {
-      const rcvdArray = d.filter(e => e.fromUser === 'user1');
-      return rcvdArray.toUser;
-    },
-  }, {
-    Header: 'Amount',
-    accessor: 'amount',
-  }, {
-    Header: 'Status',
-    accessor: 'status',
-  }, {
-    Header: 'transaction Id',
-    accessor: 'transactionId',
-  }];
-
-  switch (props.displayTab) {
-    case 0:
-      return (
-        <div>
-          <TableHead
-            head={headAll}
-          />
-          <TableData
-            data={dataAll}
-          />
-        </div>
-      );
-    case 1:
-      return (
-        <ReactTable
-          data={dataAll}
-          columns={columnsSent}
-        />
-      );
-    case 2:
-      return (
-        <ReactTable
-          data={dataAll}
-          columns={columnsRcvd}
-        />
-      );
-    default:
-      return (
-        <div>
-          Default Tab
-        </div>
-      );
-  }
-
-  // return (
-  //   Object.keys(props.transactionObject).map(keys => (
-  //     <div className="TransactionDisplayer-main">
-  //       <div className="TransactionsDisplayer-sent">
-  //         {props.transactionObject[keys].sent}
-  //       </div>
-  //       <div className="TransactionsDisplayer-amount">
-  //         {props.transactionObject[keys].amount}
-  //       </div>
-  //       <div className="TransactionsDisplayer-status">
-  //         {props.transactionObject[keys].status}
-  //       </div>
-  //       <div className="TransactionsDisplayer-id">
-  //         {props.transactionObject[keys].transactionId}
-  //       </div>
-  //     </div>
-
-  //   ))
-  // );
+  return (
+    <table width="100%" border="1px solid black">
+      {headers}
+      {allRows}
+    </table>
+  );
 };
 
-
 export default TransactionsDisplayer;
-
