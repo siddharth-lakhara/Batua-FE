3159ccb38b4fb584344fdc1f0164b81fdb37f45f
lakhara <siddharth.lakhara@gmail.com>
2018-03-11 23:25:44 +0530

    added on changeHandler to all textareas

--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -3,6 +3,38 @@ import './register.css';
 import Details from '../details/Details';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: 'sid',
+      lastName: '',
+      aadharNo: null,
+      phone: '',
+      accountNo: '',
+      userName: '',
+      password: '',
+      confirmPassword: '',
+    };
+    this.registerUser = this.registerUser.bind(this);
+  }
+
+  registerUser() {
+    const {
+      firstName, lastName, phone, accountNo, userName, password,
+    } = this.state;
+    const aadharNo = Number(this.state.aadharNo);
+    const POSTdata = {
+      firstName, lastName, phone, accountNo, userName, password, aadharNo,
+    };
+
+    fetch('/user', {
+      method: 'POST',
+      body: JSON.stringify(POSTdata),
+    })
+      .then(data => data.json())
+      .then((data) => { console.log(data); });
+  }
+
   render() {
     return (
       <div className="register-main">
@@ -11,16 +43,29 @@ class Login extends Component {
           Register
           </div>
           <div className="register-details">
-            <Details icon="person" placeholder="First Name" />
-            <Details icon="perm_identity" placeholder="Last Name" />
-            <Details icon="confirmation_number" placeholder="Aadhar number" />
-            <Details icon="account_balance_wallet" placeholder="Account Number" />
-            <Details icon="phone_iphone" placeholder="Phone Number" />
-            <Details icon="assignment_ind" placeholder="User Name" />
-            <Details icon="vpn_key" placeholder="password" />
-            <Details icon="vpn_key" placeholder="Confirm password" />
+            <Details icon="person" placeholder="FirstName" value={this.state.firstName} changeHandler={(event) => { this.setState({ firstName: event.target.value }, () => { console.log('newState: ', this.state.firstName); }); }} />
+            <Details icon="perm_identity" placeholder="Last Name" value={this.state.lastName} changeHandler={(event) => { this.setState({ lastName: event.target.value }, () => { console.log('newState: ', this.state.lastName); }); }} />
+            <Details icon="confirmation_number" placeholder="Aadhar number" value={this.state.aadharNo} changeHandler={(event) => { this.setState({ aadharNo: event.target.value }, () => { console.log('newState: ', this.state.aadharNo); }); }} />
+            <Details icon="account_balance_wallet" placeholder="Account Number" value={this.state.accountNo} changeHandler={(event) => { this.setState({ accountNo: event.target.value }, () => { console.log('newState: ', this.state.accountNo); }); }} />
+            <Details icon="phone_iphone" placeholder="Phone Number" value={this.state.phone} changeHandler={(event) => { this.setState({ phone: event.target.value }, () => { console.log('newState: ', this.state.phone); }); }} />
+            <Details icon="assignment_ind" placeholder="User Name" value={this.state.userName} changeHandler={(event) => { this.setState({ userName: event.target.value }, () => { console.log('newState: ', this.state.userName); }); }} />
+            <Details icon="vpn_key" placeholder="password" value={this.state.password} changeHandler={(event) => { this.setState({ password: event.target.value }, () => { console.log('newState: ', this.state.password); }); }} />
+            <Details icon="vpn_key" placeholder="Confirm password" value={this.state.confirmPassword} changeHandler={(event) => { this.setState({ confirmPassword: event.target.value }, () => { console.log('newState: ', this.state.confirmPassword); }); }} />
             <div className="register-button-div">
-              <button className="register-button">Register</button>
+              <button
+                className="register-button"
+                onClick={() => {
+                  if (this.state.password !== this.state.confirmPassword) {
+                    alert('Passwords don\'t match');
+                  } else {
+                    this.registerUser();
+                    // this.props.changeToRegister();
+                  }
+                }
+              }
+              >
+              Register
+              </button>
             </div>
           </div>
         </div>
